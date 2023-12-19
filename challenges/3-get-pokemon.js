6b79d699791c664d602ab25b02bdc720e41a4761
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2023-12-19 16:07:47 +0000

    Complete kata 3 - axios

---
+++ challenges/3-get-pokemon.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const getPokemon = () => {
+    return axios.get('https://pokeapi.co/api/v2/pokemon').
+    then((pokemon)=>{
+        const caughtPokemon = [];
+        pokemon.data.results.forEach(pokemon => {
+            if(caughtPokemon.length < 20){
+                caughtPokemon.push(pokemon);
+            }
+        });
+        return caughtPokemon;
+    }).then((pokemon) => {
+        console.log(pokemon);
+    })
+}
+console.log(getPokemon())
